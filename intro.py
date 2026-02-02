0513c9ad1fa924b260ec68669a0506b834a3b8ee
Noha Shehab <nohashehab.iti@gmail.com>
2026-02-02 09:22:19 +0200

    adding notes

---
+++ intro.py
@@ -0,0 +1,57 @@
+
+
+
+"""
+hello from python Day01
+today we are exploring the basics of python syntax
+and how it be used in developing different applications
+"""
+
+name = "noha"
+
+print(name)
+
+# if = "iti" # this is a comment, syntax error
+
+# print= "iti"
+
+# print("Hello from python Day01")  #TypeError: 'str' object is not callable
+
+
+#we have only line indentation
+
+ # email = "test@iti.com"  # IndentationError: unexpected indent
+
+day = 'Saturday'
+if day == 'Saturday':
+    print("Happy Python Week")
+elif day == 'Sunday':
+    print("It seems impossible until it is done ")
+else:
+    print("------------")
+
+
+
+
+
+
+# main is in the file(.py file )
+
+# where is the main function ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
